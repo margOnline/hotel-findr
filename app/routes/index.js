008edeef8dabc52b3c75f660d6b3fd9428f67d75
Margo Urey <margo@margonline.co.uk>
2016-11-01 21:29:42 +0000

    wire up css and partials

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  res.send(`
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <script src="/reload/reload.js"></script>
-    <h2>Top Hotels</h2>
-    <img src="/images/icon_sprite.png">
-    <script src="/js/behaviour.js"><script>
-  `)
+  var data = req.app.get('appData');
+
+  res.render('index', {
+    pageTitle: "Home",
+    pageID: "home",
+    countries: data["country"]
+  });
 });
 
 module.exports = router;
